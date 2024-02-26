1891ba3d99f0ef3beedba7ace387d05717b633b2
John Jang <jscolbe9675@gmail.com>
2024-02-26 10:50:41 -0500

    experimental duplicate

---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import api from "../utils/api";
+import { useNavigate } from "react-router-dom";
+
+const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [secondPassword, setSecondPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    try {
+      if (password !== secondPassword) {
+        throw new Error(
+          "the password does not match. please check your password again"
+        );
+      }
+      const response = await api.post("/user", { name, email, password });
+      if (response.status == 200) {
+        navigate("/login");
+      } else {
+        throw new Error(response.data.error);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+    event.preventDefault();
+  };
+  return (
+    <div className="display-center">
+      {error && <div className="control-red">{error}</div>}
+      <Form className="login-box" onSubmit={handleSubmit}>
+        <h1>회원가입</h1>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="string"
+            placeholder="Name"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>re-enter the password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="re-enter the password"
+            onChange={(event) => setSecondPassword(event.target.value)}
+          />
+        </Form.Group>
+
+        <Button className="button-primary" type="submit">
+          회원가입
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default RegisterPage;
